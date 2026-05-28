36aa3759d81fe919e10bd5bf2546b926e2edf8d7
Ajay Nalwade <ajaynalwade321@gmail.com>
2026-05-28 19:35:51 +0530

    fixed url

--- library-frontend/src/api/api.js
+++ library-frontend/src/api/api.js
@@ -1,6 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_URL
-
-export async function apiFetch(endpoint, options = {}) {
+const BASE_URL ="https://library-management-system-fullstack-1.onrender.com"
+  export async function apiFetch(endpoint, options = {}) {
     console.log(import.meta.env.VITE_API_URL)
   const response = await fetch(
     `${BASE_URL}${endpoint}`,
