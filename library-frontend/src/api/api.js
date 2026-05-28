9b806111246f78a403d0b71ed00e75ac8a963332
Ajay Nalwade <ajaynalwade321@gmail.com>
2026-05-28 19:20:54 +0530

    deploy

--- library-frontend/src/api/api.js
+++ library-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = import.meta.env.VITE_API_URL
 
 export async function apiFetch(endpoint, options = {}) {
-
+    console.log(import.meta.env.VITE_API_URL)
   const response = await fetch(
     `${BASE_URL}${endpoint}`,
     {
