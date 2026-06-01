15a7eeabe187ff2819dd3cee520797f550cdb630
Ajay Nalwade <ajaynalwade321@gmail.com>
2026-06-01 16:26:37 +0530

    Changes

--- library-frontend/src/pages/Dashboard.jsx
+++ library-frontend/src/pages/Dashboard.jsx
@@ -16,13 +16,7 @@ export default function Dashboard({ data, onNavigate }) {
  const navigate = useNavigate();
 
 
-  if (message) {
-    alert(
-      "Backend might take 90 s to open. Please stay on this page. After the backend starts, you will be redirected to the login page."
-    );
-    
-  }
-  setMessage(false);
+  
 
  
  useEffect(()=>{
